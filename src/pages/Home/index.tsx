2fdab50d86f3e8c965c751bc22c044f7e763379c
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-08 23:11:31 +0100

    add icons and logos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from '../../components/Header';
+
 const Home: React.FC = () => (
   <>
+    <Header />
     <h1>Movies Cenas</h1>
     <Link to="/movie-detail">Detail</Link>
   </>
--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from '../../components/Header';
+
 const Movie: React.FC = () => (
   <>
+    <Header />
     <h1>Movie Detail</h1>
     <Link to="">Home</Link>
   </>
