4207d8bc94f2bce78ba3c3e377585d70e5d98c2a
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-10 10:42:49 +0100

    update cards to show title and year on hover

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Container, CardContainer } from './styles';
+import { Container, CardContainer, HoverCardContainer } from './styles';
 
 import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
@@ -14,73 +14,102 @@ const Home: React.FC = () => (
 
     <Container>
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+
+          <div className="content">
+            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
+            <p>2018</p>
+          </div>
+          {/* <HoverCardContainer>
+            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
+            <p>2018</p>
+          </HoverCardContainer> */}
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
 
       <CardContainer>
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          alt="FastAndFurious"
-        />
+        <div className="imgBox">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+            alt="FastAndFurious"
+          />
+        </div>
       </CardContainer>
     </Container>
     <h1>Movies Cenas</h1>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,15 +5,50 @@ export const Container = styled.div`
   margin: 50px auto;
 
   display: flex;
-  justify-content: space-around;
   flex-flow: wrap;
+  justify-content: space-between;
 `;
 
 export const CardContainer = styled.div`
-  width: 20%;
+  position: relative;
+  width: 200px;
+  height: 300px;
+  box-shadow: 0 30px 30px rgba(0, 0, 0, 0.5);
+
+  .imgBox {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      transition: 0.5s;
+    }
+  }
+
+  &:hover .imgBox img {
+    opacity: 0.2;
+  }
+
+  &:hover .content {
+    position: absolute;
+    bottom: 20px;
+    left: 10%;
+    width: 80%;
+    color: #fff;
+  }
+
+  /* width: 20%; */
   margin-bottom: 50px;
 
-  @media (max-width: 1345px) {
+  /* @media (max-width: 1345px) {
     width: 25%;
   }
   @media (max-width: 1075px) {
@@ -26,5 +61,10 @@ export const CardContainer = styled.div`
   @media (max-width: 540px) {
     width: 100%;
     flex: 0 1 calc(25% - 1em);
-  }
+  } */
+`;
+
+export const HoverCardContainer = styled.div`
+  background: rgba(0, 0, 0, 0.8);
+  color: #fff;
 `;
