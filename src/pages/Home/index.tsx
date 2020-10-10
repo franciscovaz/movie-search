e38e6cf4d6baff3940815e5fbf857551ff439be2
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-10 21:54:27 +0100

    hide or show illustration

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,187 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Container, CardContainer } from './styles';
+import { Container, NoInfoContainer } from './styles';
 
-import Icon from '../../assets/Icons/icon-heart-white.svg';
+import NoInfoImg from '../../assets/Illustrations/illustration-empty-state.png';
 
 import Header from '../../components/Header';
 import SearchBar from '../../components/SearchBar';
+import MovieCard from '../../components/MovieCard';
 
-const Home: React.FC = () => (
-  <>
-    <Header />
+const Home: React.FC = () => {
+  const [moviesSearched, setMoviesSearched] = useState(false);
+  return (
+    <>
+      <Header />
 
-    <SearchBar />
+      <SearchBar />
 
-    <Container>
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-
-          <div className="favouritesIcon">
-            <img src={Icon} alt="Add to Favourites" />
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-
-          <div className="favouritesIcon">
-            <img src={Icon} alt="Add to Favourites" />
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-
-          <div className="favouritesIcon">
-            <img src={Icon} alt="Add to Favourites" />
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-
-          <div className="favouritesIcon">
-            <img src={Icon} alt="Add to Favourites" />
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-
-          <div className="favouritesIcon">
-            <img src={Icon} alt="Add to Favourites" />
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-
-          <div className="favouritesIcon">
-            <img src={Icon} alt="Add to Favourites" />
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-        </div>
-      </CardContainer>
-
-      <CardContainer>
-        <div className="imgBox">
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="FastAndFurious"
-          />
-
-          <div className="content">
-            <p>Titulo do filme e cenas do mal a man como novo nos cinemas</p>
-            <span>2018</span>
-          </div>
-        </div>
-      </CardContainer>
-    </Container>
-    <h1>Movies Cenas</h1>
-    <Link to="/movie-detail">Detail</Link>
-  </>
-);
+      {moviesSearched ? (
+        <Container>
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+        </Container>
+      ) : (
+        <NoInfoContainer>
+          <img src={NoInfoImg} alt="No movies searched" />
+          {/* <Link to="/movie-detail">Detail</Link> */}
+        </NoInfoContainer>
+      )}
+    </>
+  );
+};
 
 export default Home;
