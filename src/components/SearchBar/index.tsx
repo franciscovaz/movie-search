18f94e75ff92ba95badf54f6feecbdd2064e7d8d
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-11 21:58:21 +0100

    deal with api response on input

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,16 +1,51 @@
-import React, { useState, useCallback, ChangeEvent } from 'react';
+import React, { useState, useCallback, ChangeEvent, useEffect } from 'react';
 
 import Icon from '../../assets/Icons/icon-magnifier-grey.svg';
+import api from '../../services/api';
 
 /* import { FiSearch } from 'react-icons/fi'; */
 
 import { Container } from './styles';
 
+interface MovieResponseProps {
+  Response: string;
+  Search: [MovieProps];
+}
+
+interface MovieProps {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
 const SearchBar: React.FC = () => {
   const [inputMovie, setInputMovie] = useState('');
+  const [moviesSearched, setMoviesSearched] = useState<MovieProps[]>([]);
+
   const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setInputMovie(e.target.value);
   }, []);
+
+  useEffect(() => {
+    if (inputMovie) {
+      api
+        .get<MovieResponseProps>(
+          `?apikey=${process.env.REACT_APP_API_OMDb_SECRET}&s=${inputMovie}`,
+        )
+        .then(response => {
+          console.log(response.data);
+          if (response.data.Response === 'False') {
+            setMoviesSearched([]);
+          }
+          setMoviesSearched(response.data.Search);
+        })
+        .catch(err => {
+          console.log('Do something about this error: ', err);
+        });
+    }
+  }, [inputMovie]);
 
   return (
     <Container>
