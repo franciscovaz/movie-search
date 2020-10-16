e65c64bb40e6b0875c46508280441467aeef7a30
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-16 18:55:20 +0100

    Add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+
 import GlobalStyle from './styles/global';
 import Routes from './pages/routes';
+import store from './store';
 
 const App: React.FC = () => (
-  <>
+  <Provider store={store}>
     <GlobalStyle />
     <Routes />
-  </>
+  </Provider>
 );
 
 export default App;
