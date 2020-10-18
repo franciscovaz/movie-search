942522a7985daef43845f4401d063b26d5a7345b
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-18 10:09:31 +0100

    changed returned interface of axios call

--- src/store/modules/movies/moviesSagas.ts
+++ src/store/modules/movies/moviesSagas.ts
@@ -6,19 +6,18 @@ import {
   addMoviesSearchedSuccess,
 } from './moviesActions';
 
-import { ActionTypes, IMovieResponseProps } from './moviesTypes';
+import { ActionTypes, IMoviesState } from './moviesTypes';
 import api from '../../../services/api';
 
 type GetMoviesSearched = ReturnType<typeof addMoviesSearchedRequest>;
 
 function* getMoviesSearched({ payload }: GetMoviesSearched) {
-  const apiResponse: AxiosResponse<IMovieResponseProps> = yield call(
+  const apiResponse: AxiosResponse<IMoviesState> = yield call(
     api.get,
     `?apikey=${process.env.REACT_APP_API_OMDb_SECRET}&s=${payload.movieName}`,
   );
 
   if (apiResponse.data.Response === 'True') {
-    console.log('Entrei', apiResponse.data);
     yield put(addMoviesSearchedSuccess(apiResponse.data));
   }
 }
