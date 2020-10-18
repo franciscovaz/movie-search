1ed3a66906058a699e5278d5a7914cbd939a24e1
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-18 10:08:11 +0100

    export IState

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,13 @@ import createSagaMiddleware from 'redux-saga';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import { IMoviesState } from './modules/movies/moviesTypes';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
+
+export interface IState {
+  movies: IMoviesState;
+}
 
 const sagaMiddleware = createSagaMiddleware();
 
