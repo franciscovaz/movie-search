71d800a2fb38edcc9491d634959ed03146302500
Francisco Vaz <ftomasvaz@gmail.com>
2020-10-22 21:48:39 +0100

    add icont to go to home

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import goBackArrow from '../../assets/Icons/icon-arrow-grey.svg';
+
 import Header from '../../components/Header';
 
 const Movie: React.FC = () => (
   <>
     <Header />
-    <h1>Movie Detail</h1>
-    <Link to="">Home</Link>
+
+    <Link to="">
+      <img src={goBackArrow} alt="Go home" />
+    </Link>
   </>
 );
 export default Movie;
